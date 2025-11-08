48a1fdc8b3064fca281293d743d084eaa976cd85
WilO22 <wilmer.ogosi.22@gmail.com>
2025-11-08 18:19:59 -0500

    feat(auth): implementa flujo de Login con Google exitoso (Paso 1 y 2)

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAUcA7_gjFtiBixVvO2fL30t6Ko3my48fk",
+    authDomain: "proyecto-final-ecommerce-56d99.firebaseapp.com",
+    projectId: "proyecto-final-ecommerce-56d99",
+    storageBucket: "proyecto-final-ecommerce-56d99.firebasestorage.app",
+    messagingSenderId: "870862791397",
+    appId: "1:870862791397:web:e0feebea99d93bcbd0ea0c"
+  }
+};
