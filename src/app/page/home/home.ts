86bc1152f5244e5f34b1a144778a9f22cda4289e
yaquitadeveloper <yaquitadeveloper@gmail.com>
2025-11-08 23:17:22 -0500

    haciendo pruebas de las cards en las paginas home

--- src/app/page/home/home.ts
+++ src/app/page/home/home.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from '../../components/product-card/product-card';
+import { Product } from '../../core/models/product.interface';
+import { ProductService } from '../../core/service/product';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ProductCardComponent],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrls: ['./home.css'],
 })
-export class Home {
+export class Home implements OnInit {
+  featuredProducts: Product[] = [];
 
+  constructor(private productService: ProductService) {} // 👈 Inyectar el servicio
+
+  ngOnInit(): void {
+    // Cargar productos usando el servicio
+    this.productService.getFeaturedProducts().subscribe({
+      next: (products) => {
+        this.featuredProducts = products;
+        console.log('Productos cargados:', this.featuredProducts.length);
+      },
+      error: (error) => {
+        console.error('Error al cargar productos:', error);
+      },
+    });
+  }
 }
