f557092b2933c0ae95f6dd4bf1257c05db174d51
Dennis Miranda <dennise.miranda.15@gmail.com>
2025-11-09 14:39:50 -0600

    Add sidebar responsive

--- src/app/feature/admin/components/sidebar/sidebar.ts
+++ src/app/feature/admin/components/sidebar/sidebar.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { House, LogOut, LucideAngularModule, Package2, Shapes, ShoppingCart } from 'lucide-angular';
+import {
+  House,
+  LogOut,
+  LucideAngularModule,
+  Menu,
+  Package2,
+  Shapes,
+  ShoppingCart,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [LucideAngularModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, LucideAngularModule, RouterLink, RouterLinkActive],
 
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
@@ -15,4 +24,38 @@ export class Sidebar {
   categoryIcon = Shapes;
   orderIcon = ShoppingCart;
   logoutIcon = LogOut;
+  menuIcon = Menu;
+
+  isOpen = false;
+  isMobile = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkScreenSize();
+  }
+
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    this.isMobile = window.innerWidth < 768; // md breakpoint
+    if (!this.isMobile) {
+      this.isOpen = true;
+    } else {
+      this.isOpen = false;
+    }
+  }
+
+  toggleMenu() {
+    if (this.isMobile) {
+      this.isOpen = !this.isOpen;
+    }
+  }
+
+  closeMenu() {
+    if (this.isMobile) {
+      this.isOpen = false;
+    }
+  }
 }
