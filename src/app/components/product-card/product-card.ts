d5c1c87c28fa34c7bc508201669f872e1c64b032
yaquitadeveloper <yaquitadeveloper@gmail.com>
2025-11-14 02:26:05 -0500

    hacer que la card de producto sea clickeable para reedirigir al detalle del producto mismo

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { Product } from '../../core/models/product';
 import { LucideAngularModule, ShoppingCart } from 'lucide-angular';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule, LucideAngularModule],
+  imports: [CommonModule, LucideAngularModule, RouterLink],
   templateUrl: './product-card.html',
   styleUrls: ['./product-card.css'],
 })
