49e8e57c89953549517d16b91d50b589dffd3438
yaquitadeveloper <yaquitadeveloper@gmail.com>
2025-11-13 19:55:38 -0500

    creando constantes globales de las categorias

--- src/app/core/constants/categories.ts
+++ src/app/core/constants/categories.ts
@@ -1,4 +1,4 @@
-export const CATEGORIES = {
+export const CATEGORIES: Record<number, string> = {
   1: 'Electrónica',
   2: 'Deportes',
   3: 'Accesorios',
@@ -9,6 +9,19 @@ export const CATEGORIES = {
   8: 'Herramientas',
   9: 'Muebles',
   10: 'Audio',
-} as const;
+};
 
-export type CategoryId = keyof typeof CATEGORIES;
+// export const CATEGORIES = {
+//   1: 'Electrónica',
+//   2: 'Deportes',
+//   3: 'Accesorios',
+//   4: 'Computación',
+//   5: 'Tecnología',
+//   6: 'Hogar',
+//   7: 'Moda',
+//   8: 'Herramientas',
+//   9: 'Muebles',
+//   10: 'Audio',
+// } as const;
+
+// export type CategoryId = keyof typeof CATEGORIES;
