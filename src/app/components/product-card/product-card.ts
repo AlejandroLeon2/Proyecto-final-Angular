924feccb69286f2b3efea048c3accc8ac7b88c46
yaquitadeveloper <yaquitadeveloper@gmail.com>
2025-11-14 01:45:10 -0500

    refactorizando la logica del componente card incorporando el icono del carrito con la liberia lucide

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../core/models/product';
+import { LucideAngularModule, ShoppingCart } from 'lucide-angular';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './product-card.html',
   styleUrls: ['./product-card.css'],
 })
 export class ProductCardComponent {
   @Input() product!: Product; // Recibe el producto del componente padre
+
+  shoppingCartIcon = ShoppingCart;
 
   /**
    * Formatea la fecha a formato legible
